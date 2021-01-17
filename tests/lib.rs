2671011c6a407eef36b0d727011e181af24355e4
zkonge <zkonge@outlook.com>
2021-01-18 02:16:48 +0800

    Replace deprecated library block_cipher with cipher

--- tests/lib.rs
+++ tests/lib.rs
@@ -2,8 +2,8 @@
 //! http://www.cix.co.uk/~klockstone/teavect.htm
 #![no_std]
 
-use block_cipher::new_test;
+use cipher::block_cipher_test;
 
-new_test!(tea16_test, "tea16", tea_soft::Tea16);
-new_test!(tea32_test, "tea32", tea_soft::Tea32);
-new_test!(tea64_test, "tea64", tea_soft::Tea64);
+block_cipher_test!(tea16_test, "tea16", tea_soft::Tea16);
+block_cipher_test!(tea32_test, "tea32", tea_soft::Tea32);
+block_cipher_test!(tea64_test, "tea64", tea_soft::Tea64);
